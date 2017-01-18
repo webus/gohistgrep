1e36938da6d61fcc4bcb4b5fed9140aedb561ef5
webus <webusnix@gmail.com>
2017-01-18 11:37:47 +0300

    added default like operators for simple words

--- search.go
+++ search.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"strconv"
 	log "github.com/Sirupsen/logrus"
 )
 
 func SearchCommand(command string) {
+	if !strings.HasPrefix(command, "%") && !strings.HasSuffix(command,"%") {
+		command = "%" + command + "%"
+	}
 	logs := []Log{}
 	err := db.Select(&logs, "SELECT * FROM log WHERE LOWER(command) LIKE $1 ORDER BY popularity DESC", command)
 	if err != nil {
