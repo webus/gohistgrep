f764402d45a047868f448b6b34150180741fbc81
webus <webusnix@gmail.com>
2017-01-23 09:31:33 +0300

    added short version, like: word+word+word etc...

--- search.go
+++ search.go
@@ -7,9 +7,24 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
+
 func SearchCommand(command string) {
-	if !strings.HasPrefix(command, "%") && !strings.HasSuffix(command,"%") {
-		command = "%" + command + "%"
+	if strings.Contains(command, "+") {
+		data := strings.Split(command, "+")
+		out := ""
+		if len(data) > 0 {
+			out += "%"
+		}
+		for _, d := range data {
+			if d != "" {
+				out += d + "%"
+			}
+		}
+		command = out
+	} else {
+		if !strings.HasPrefix(command, "%") && !strings.HasSuffix(command,"%") {
+			command = "%" + command + "%"
+		}
 	}
 	logs := []Log{}
 	err := db.Select(&logs, "SELECT * FROM log WHERE LOWER(command) LIKE $1 ORDER BY popularity DESC", command)
